710d6733e1b1fb205a83bcb279d86cc628169ee8
benvens <simon.benvenutto@gmail.com>
2021-07-04 16:17:26 +0100

    tweaks

--- fashion_train_tf.py
+++ fashion_train_tf.py
@@ -1,8 +1,34 @@
 import tensorflow as tf
+from tensorflow import keras
 
-device_name = 'GPU'
+device_name = 'GPU' # 'GPU'
 batch_size=64
 num_epochs=10
+
+
+class ResidualBlock(keras.layers.Layer):
+    """A residual block (ResNet) for image processing.
+    """
+
+
+def __init__(self):
+    super().__init__()
+
+    self.conv1 = tf.keras.layers.Conv2D(activation='relu')
+    self.relu1 = tf.keras.layers.ReLU()
+    self.conv2 = tf.keras.layers.Conv2D(activation='relu')
+    self.add = tf.keras.layers.Add()
+    self.relu2 = tf.keras.layers.ReLU()
+
+
+def call(self, inputs):
+    x = self.conv1(inputs)
+    x = self.relu1(x)
+    x = self.conv2(x)
+    x = self.add(x, inputs)
+    x = self.relu2(x)
+    return x
+
 
 device_id = f'/device:{device_name}:0'
 with tf.device(device_id):
@@ -14,18 +40,27 @@ with tf.device(device_id):
     train_images = train_images / 255.0
     test_images = test_images / 255.0
 
-    class ResidualBlock(keras.layers.Leyer):
-        """A residual block (ResNet) for image processing.
-        """
-
-
-
     model = tf.keras.Sequential([
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
+        ResidualBlock(input_shape=(28, 28)),
         tf.keras.layers.Flatten(input_shape=(28, 28)),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(10)
     ])
+
+    model.summary()
 
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
