23bc78859bf6eca5060af2729d2a8ae2017820f7
benvens <simon.benvenutto@gmail.com>
2021-07-06 07:44:35 +0100

    tweaks

--- fashion_train_tf.py
+++ fashion_train_tf.py
@@ -1,33 +1,25 @@
 import tensorflow as tf
 from tensorflow import keras
 
-device_name = 'GPU' # 'GPU'
-batch_size=64
-num_epochs=10
+device_name = 'CPU'  # 'GPU'
+batch_size = 64
+num_epochs = 10
 
 
 class ResidualBlock(keras.layers.Layer):
     """A residual block (ResNet) for image processing.
     """
+    def __init__(self, **kwargs):
+        super(ResidualBlock, self).__init__(kwargs)
+        self.conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+        self.conv2 = tf.keras.layers.Conv2D(32, (3, 3))
 
-
-def __init__(self):
-    super().__init__()
-
-    self.conv1 = tf.keras.layers.Conv2D(activation='relu')
-    self.relu1 = tf.keras.layers.ReLU()
-    self.conv2 = tf.keras.layers.Conv2D(activation='relu')
-    self.add = tf.keras.layers.Add()
-    self.relu2 = tf.keras.layers.ReLU()
-
-
-def call(self, inputs):
-    x = self.conv1(inputs)
-    x = self.relu1(x)
-    x = self.conv2(x)
-    x = self.add(x, inputs)
-    x = self.relu2(x)
-    return x
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        x = tf.add(x, inputs)
+        x = tf.nn.relu(x)
+        return x
 
 
 device_id = f'/device:{device_name}:0'
@@ -40,26 +32,26 @@ with tf.device(device_id):
     train_images = train_images / 255.0
     test_images = test_images / 255.0
 
-    model = tf.keras.Sequential([
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        ResidualBlock(input_shape=(28, 28)),
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10)
-    ])
+    # model = tf.keras.Sequential([
+    #     ResidualBlock(input_shape=(28, 28)),
+    #     tf.keras.layers.Flatten(input_shape=(28, 28)),
+    #     tf.keras.layers.Dense(128, activation='relu'),
+    #     tf.keras.layers.Dense(128, activation='relu'),
+    #     tf.keras.layers.Dense(10)
+    # ])
 
+    # model = tf.keras.Sequential([
+    #     ResidualBlock(input_shape=(28, 28)),
+    #     # # ResidualBlock(input_shape=(28, 28)),
+    #     # # ResidualBlock(input_shape=(28, 28)),
+    #     # ResidualBlock(input_shape=(28, 28)),
+    #     tf.keras.layers.Flatten(),
+    #     tf.keras.layers.Dense(10)
+    # ])
+
+    model = tf.keras.applications.ResNet50V2()
+
+    # model.build(input_shape=(28, 28))
     model.summary()
 
     model.compile(optimizer='adam',
